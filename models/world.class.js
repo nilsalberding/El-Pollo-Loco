ded1f9bf6f1a2e83e185551e40dd446ccac993df
Nils Alberding-Schulz <nilsalberding@web.de>
2025-08-11 09:24:13 +0200

    add CheckCollisions() to class World()

--- models/world.class.js
+++ models/world.class.js
@@ -1,3 +1,4 @@
+import { IntervalHub } from "../js/intervall_hub.class.js";
 import { Pix } from "../js/pix.class.js";
 import { level1 } from "../level/level1.js";
 import { Character } from "../models/character.class.js";
@@ -6,68 +7,93 @@ import { BackgroundObject } from "./background_objects.class.js";
 import { Cloud } from "./cloud.class.js";
 import { Level } from "./level.class.js";
 
+
+
 export class World {
 
     character = new Character();
     level = level1;
-    // enemies = level1.enemies;
-    // clouds = level1.clouds;
-    // backgroundObjects = level1.backgroundObjects;
 
-    canvas;    
+    canvas;
     ctx;
     camera_x = 0;
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
-        this.canvas = canvas;        
+        this.canvas = canvas;
         this.draw();
         this.setWorld();
+        IntervalHub.startInterval(this.checkCollisions, 1000/5);
     }
 
     setWorld() {
         this.character.world = this;
     }
 
+    checkCollisions = () => {
+        this.level.enemies.forEach((enemy) => {
+            if(this.character.isColliding(enemy)) {
+                this.character.hit();
+                console.log(this.character.health);
+                
+            }
+        })
+    }
+
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        
+
         this.ctx.translate(this.camera_x, 0);
-        
+
         this.addObjectsToMap(this.level.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.enemies);
         this.addObjectsToMap(this.level.clouds);
-        
+
         this.ctx.translate(-this.camera_x, 0);
-
-        // draw()wird immer wieder aufgerufen
-        // const self = this;
-        // requestAnimationFrame(function () {
-        //     self.draw();
-        // })
-
         requestAnimationFrame(() => this.draw());
     }
 
     addToMap(mO) {
-        if(mO.otherDirection) {
-            this.ctx.save();
-            this.ctx.translate(mO.width, 0);
-            this.ctx.scale(-1, 1);
-            mO.x = mO.x * -1;
+        if (mO.otherDirection) {
+            this.flipImage(mO);
         }
+
+        this.showRectangle(mO);
+
         this.ctx.drawImage(mO.img, mO.x, mO.y, mO.width, mO.height)
-        if(mO.otherDirection) {
-            this.ctx.restore();
-            mO.x = mO.x * -1;
+        if (mO.otherDirection) {
+            this.flipImageBack(mO);
         }
     };
+
+    showRectangle(mO) {
+        if (mO instanceof Character || mO instanceof Chicken) {
+            this.ctx.beginPath();
+            this.ctx.lineWidth = '3';
+            this.ctx.strokeStyle = 'blue';
+            this.ctx.rect(mO.x, mO.y, mO.width, mO.height);
+            this.ctx.stroke();
+        }
+    }
 
     addObjectsToMap(object) {
         object.forEach(o => {
             this.addToMap(o);
-        })};
+        })
+    };
+
+    flipImage(mO) {
+        this.ctx.save();
+        this.ctx.translate(mO.width, 0);
+        this.ctx.scale(-1, 1);
+        mO.x = mO.x * -1;
+    }
+
+    flipImageBack(mO) {
+        this.ctx.restore();
+        mO.x = mO.x * -1;
+    }
 }
 
